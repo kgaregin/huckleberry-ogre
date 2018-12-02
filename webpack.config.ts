16e462fc79376ed778f50503292618dbc7510ebf
HuckleberryOgre <kgaregin@gmail.com>
2018-12-02 22:53:58 +0300

    production environment managed

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+import MinifyPlugin  from 'babel-minify-webpack-plugin';
 
 const tsFrontDirs = [
     path.resolve(__dirname, 'src/client'),
@@ -14,14 +14,13 @@ const webpackConfiguration: webpack.Configuration = {
     stats: {
         colors: true
     },
-    mode: 'development',
+    mode: 'production',
     entry: './src/client/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
     },
-    devtool: 'source-map',
     resolve: {
         //ToDo: investigate, why this is not enough to resolve imports that are relative to src directory
         modules: [path.resolve(__dirname, 'src'), 'node_modules'],
@@ -29,11 +28,6 @@ const webpackConfiguration: webpack.Configuration = {
     },
     module: {
         rules: [{
-            enforce: 'pre',
-            test: /\.ts|\.tsx$/,
-            loader: 'source-map-loader',
-            include: tsFrontDirs
-        }, {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
         }, {
@@ -64,16 +58,8 @@ const webpackConfiguration: webpack.Configuration = {
             title: 'Huckleberry Ogre welcomes you, stranger!',
             template: 'index_template.ejs'
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin([
-            {from: 'node_modules/react/umd/react.development.js', to: 'react.development.js'},
-            {from: 'node_modules/react-dom/umd/react-dom.development.js', to: 'react-dom.development.js'},
-        ])
-    ],
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM'
-    }
+        new MinifyPlugin()
+    ]
 };
 
 module.exports = webpackConfiguration;
